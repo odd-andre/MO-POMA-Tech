1e5d0ff740edfb061b6c2c804bda8d35349dd633
Odd Andre Vatne <oddi160@gmail.com>
2018-09-04 10:12:25 +0200

    Add sql-handler and work on jdbc connection

---
+++ src/java/Classes/SqlHandler.java
@@ -0,0 +1,95 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Classes;
+import java.io.PrintWriter;
+import java.sql.*; 
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+/**
+ *
+ * @author oddandre
+ */
+public class SqlHandler {
+    String select;
+    String where;
+    String from;
+    PrintWriter out;
+    Connection conn;        // Must be defined here as class variables, get their value in the login method
+    Statement stmt;    
+    
+    
+    public SqlHandler (PrintWriter out){
+        this.out = out;
+        this.connectDb(out);
+    }
+    private void connectDb(PrintWriter out){
+        try {
+             // Step 1: Allocate a database 'Connection' object
+             Context cont = new InitialContext();
+             DataSource ds = (DataSource)cont.lookup("java:comp/env/jdbc/LocalhostDS");  
+             //DataSource ds = (DataSource)cont.lookup("jdbc/LocalhostDS");
+             conn =  ds.getConnection();
+
+             // Step 2: Allocate a 'Statement' object in the Connection
+             stmt = conn.createStatement();
+            }
+            catch (SQLException ex ) {
+                out.println("Not connected to database " +ex);
+            }
+            catch (NamingException nex) {
+                out.println("Not correct naming" + nex);
+            }
+    }
+    public void select(String selectString){
+        this.select = selectString;
+    }
+    public void where(String whereString){
+    
+    }
+    public void runSelect(){
+        PreparedStatement selectString;
+        try {
+            selectString = conn.prepareStatement("SELECT ? FROM ? WHERE ?");
+            selectString.setString(1,this.select);
+            selectString.setString(2,this.from);
+            selectString.setString(3,this.where);
+            
+            selectString.executeQuery();
+        } // end try     
+          catch (SQLException ex) {
+                out.println("Ikke lagre i DB " +ex);
+        }
+    }
+    
+    public void closeConnection(){
+        try {
+        conn.close();
+        
+        }
+        catch (SQLException ex) {
+                System.out.println("Ikke lukke DB " +ex);
+        }
+    }
+    public void commit(){
+        try {           
+             conn.commit();
+         } // end try     
+          
+             catch (SQLException ex) {
+                System.out.println("Ikke close DB " +ex);
+                    
+        }
+    }
+    public void clearState(){
+        this.select = "";
+        this.where = "";
+        this.from = "";
+    }
+    
+}
