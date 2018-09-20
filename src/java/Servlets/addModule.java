3cfeecdad4410873969ea4c643ede7606f03b1ea
PetrN <netp02@vse.cz>
2018-09-20 13:45:25 +0200

    Solving troubles

--- src/java/Servlets/addModule.java
+++ src/java/Servlets/addModule.java
@@ -5,53 +5,77 @@
  */
 package Servlets;
 
+import Classes.SqlHandler;
+import Entities.Student;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+import static javax.swing.JOptionPane.showMessageDialog;
 
 /**
  *
  * @author Petr
  */
-@WebServlet(name = "addModule", urlPatterns = {"/addModule"})
+@WebServlet(name = "AddModule", urlPatterns = {"/AddModule"})
 public class addModule extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+    Connection conn;
+
+    Statement pst; 
+  protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException, NamingException, SQLException {
         response.setContentType("text/html;charset=UTF-8");
-		 try {String query="insert into Module (name,deadline,learning_Goals) values (?,?,?,?)";
-			pst.setString(1,name.getText());
-			pst.setString(2,deadline.getText());
-			pst.setString(3,learning_Goals.getText());
+        try (PrintWriter out = response.getWriter()) {
+            // Get the path
+            String path = request.getPathInfo();
+            // getPathInfo includes the / after showStudent, remove it
+            String requestedStudent = path.replace("/", "");
+            //Create a sqlHandler to run database queries
+            SqlHandler sqlHdl = new SqlHandler(out);
+            //Queries return as ResultSets so we have to store it as such
+            ResultSet rst = sqlHdl.getStudent(requestedStudent);
+            
+                  Context cont = new InitialContext();
+              DataSource ds = (DataSource)cont.lookup("java:comp/env/jdbc/localhostDS");  
+              //DataSource ds = (DataSource)cont.lookup("jdbc/LocalhostDS");
+		  conn =  ds.getConnection();
+                   name =
+		 try {String query="insert into Module (id,name,deadline,learning_Goals) values (?,?,?,?)";
+			pst.setString(1,id.getText());
+			pst.setString(2,name.getText());
+			pst.setString(3,deadline.getText());
+			pst.setString(4,learning_Goals.getText());
 			
 			pst.execute();
 			
-			JOptionPane.showMessageDialog(null, "Zaměstnanec vytvořen");
-			pst.close();
-			((Node)event.getSource()).getScene().getWindow().hide();
-			
-			
-} catch (SQLException e) {
-	
-	// TODO Auto-generated catch block
-	e.printStackTrace();
-}
-}
+			showMessageDialog(null, "Module created");
+		
+            }
+            catch (SQLException ex) {
+                out.println("Ikke hentet fra DB " +ex);
+            }
+            //Put data into the requset for the next page allowing us to use it.
+            request.setAttribute("students", module);
+            //Get the jsp file where we have put our html
+            RequestDispatcher view = request.getRequestDispatcher("/Users/Addmodule.jsp");
+            //Send our data from request into the jsp file
+            view.forward(request,response);
         }
     }
 
@@ -91,7 +115,7 @@ public class addModule extends HttpServlet {
      */
     @Override
     public String getServletInfo() {
-        return "Short description";
+        return "A servlet to get a students information";
     }// </editor-fold>
 
 }
