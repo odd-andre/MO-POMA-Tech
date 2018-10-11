701bdd62723cdbc63a7606088d86ec2c279a6e2d
seawaterz <35767365+seawaterz@users.noreply.github.com>
2018-10-11 11:25:40 +0200

    Update Module.java

--- src/java/Entities/Module.java
+++ src/java/Entities/Module.java
@@ -5,71 +5,89 @@
  */
 package Entities;
 
+import Classes.SqlHandler;
+import java.io.PrintWriter;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 /**
  *
  * @author oddandre
  */
 public class Module {
-    private String name;
-    private Integer id;
-    private String deadline;
-    private String learning_Goals;
+    String name;
+    Integer id;
+    String deadline;
+    Integer tId;
+    String learnGl;
 
+   /*Constructor for the class*/
+   public Module (String name, String deadline, String learnGl, Integer id, Integer tId){
+       
+       this.deadline = deadline;
+       this.id = id;
+       this.learnGl = learnGl;
+       this.tId = tId;
+       this.name = name;
+   }
 
+    /*public Module() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 
-	public Module (String name, Integer id, String deadline , String learning_Goals)
-		{
-			
-			this.name = name;
-			this.id = id;
-			this.deadline = deadline;
-			this.learning_Goals = learning_Goals;
-		}
-     
-
-public String getName() 
-		{
-			return name;
-		}
-		
-		public Integer getId() 
-		{
-			return id;
-		}
-		
-		public String deadline() 
-		{
-			return deadline;
-		}
-		
-		public String getLearning_Goals() 
-		{
-			return learning_Goals;
-		}
-		
-	
-		
-		public void setName(String jmeno) 
-		{
-			this.name = jmeno;
-		}
-		
-		public void setId(Integer id) 
-		{
-			this.id = id;
-		}
-		
-		public void setDeadline(String deadline) 
-		{
-			this.deadline = deadline;
-		}
-		
-
-		
-		public void setLearning_Goals(String learning_Goals) 
-		{
-			this.learning_Goals = learning_Goals;
-		}
-		
-
-}
+    public Module(String mName, Integer mId, String mDeadline, String mLearnGl, Integer tId) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }*/
+    
+   
+   /*These are the methods through them we will get our results.*/
+   public String getName(){ 
+       return this.name; 
+   }
+   
+   public String getDeadline(){ 
+       return this.deadline; 
+   }
+   
+   public Integer getID(){ 
+       return this.id; 
+   }
+   
+   public String getGoals(){
+       return this.learnGl;
+   }
+   
+   public Integer getTeacherID(){ 
+       return this.tId; 
+   }
+   
+   public void getModuleDetail(Integer id,PrintWriter out){
+        SqlHandler sqlHdl = new SqlHandler(out);
+        ResultSet rst = sqlHdl.getModule(id);
+         try {
+            int rowCount = 0;
+                while(rst.next()) {   // Move the cursor to the next row, return false if no more row
+                    this.name = rst.getString("name");
+                    this.id   = rst.getInt("module_Id");
+                    this.tId = rst.getInt("teacher_Id");
+                    this.deadline = rst.getString("deadline");
+                    this.learnGl = rst.getString("learning_Goals");
+                    
+                    ++rowCount;
+                 }  // end while
+            }
+            catch (SQLException ex) {
+                out.println("Ikke hentet fra DB " +ex);
+            }
+    }
+   
+   public void createModule(PrintWriter out, String name, Integer id, String deadline, String learnGl, Integer tID){
+       SqlHandler sqlhndl = new SqlHandler(out);
+       sqlhndl.insertModule(id, name, tID, deadline, learnGl);
+  
+   }
+   //to save using printwriter object.
+   public void save(PrintWriter out){
+   }
+}// slutt
+    
