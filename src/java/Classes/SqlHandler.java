e1e29375b37a7d103a61c76723572cbcd5b1923c
seawaterz <35767365+seawaterz@users.noreply.github.com>
2018-10-11 11:26:47 +0200

    Update SqlHandler.java

--- src/java/Classes/SqlHandler.java
+++ src/java/Classes/SqlHandler.java
@@ -30,6 +30,10 @@ public class SqlHandler {
         this.out = out;
         this.connectDb(out);
     }
+
+    public SqlHandler() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     private void connectDb(PrintWriter out){
         try {
              // Step 1: Allocate a database 'Connection' object
@@ -92,11 +96,11 @@ public class SqlHandler {
                     
         }
     }
-    public ResultSet getStudent(String id){
+    public ResultSet viewModule(String id){
         PreparedStatement selectString;
         try {
-            selectString = conn.prepareStatement("SELECT user_Id,adress,email FROM User WHERE user_Id = ?");
-            selectString.setString(1, id);
+            selectString = conn.prepareStatement("SELECT module_Id,name,deadline,teacher_Id,learning_Goals FROM Modules WHERE module_Id =2");
+            //selectString.setString(1, id);
             
             return selectString.executeQuery();
         } // end try     
@@ -106,10 +110,70 @@ public class SqlHandler {
         return null;
     }
     
+    public ResultSet getModule(Integer id){
+        PreparedStatement selectString;
+        try {
+            selectString = conn.prepareStatement("SELECT module_Id,name,deadline,teacher_Id,learning_Goals " +
+                    "FROM Modules " +
+                    "WHERE module_Id = ?");
+            selectString.setInt(1, id);
+            
+            return selectString.executeQuery();
+        } // end try     
+        catch (SQLException ex) {
+             out.println("Ikke lagre i DB " +ex);
+        }
+        return null;
+    }
+    
+    public void insertModule(Integer id, String name, Integer tID, String deadline, String goals){
+    PreparedStatement selectString;
+        try {
+            selectString = conn.prepareStatement("INSERT INTO Modules "
+                    + "(module_Id, teacher_Id, deadline, learning_Goals) "
+                    + "VALUES (?, ?, ?, ?,)");
+            
+            selectString.setInt(1, id);
+            selectString.setString(1, name);
+            selectString.setInt(1, tID);
+            selectString.setString(1, deadline);
+            selectString.setString(1, goals);
+           // selectString.setInt("name1",1,11,"2018-09-28","Learn something 1");
+            selectString.executeUpdate();
+        } // end try
+        catch (SQLException ex) {
+            out.println("Ikke lagre i DB " +ex);
+        }
+    }
+    
     public void clearState(){
         this.select = "";
         this.where = "";
         this.from = "";
     }
     
+    
+    // @Resource DataSource LocalhostDS;
+    public Connection getConnection(PrintWriter out) {
+        try {
+         // Step 1: Allocate a database 'Connection' object
+         Context cont = new InitialContext();
+         DataSource ds = (DataSource)cont.lookup("java:comp/env/jdbc/localhostDS");  
+         //DataSource ds = (DataSource)cont.lookup("jdbc/LocalhostDS");
+         conn =  ds.getConnection();
+ 
+         // Step 2: Allocate a 'Statement' object in the Connection
+         stmt = conn.createStatement();
+        }
+        catch (SQLException ex ) {
+            out.println("Not connected to database " +ex);
+        }
+        catch (NamingException nex) {
+            out.println("Not correct naming" + nex);
+        }
+            
+        return conn;
+    }
+    
 }
+
