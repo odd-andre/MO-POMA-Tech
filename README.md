08159500fd8c291fec450b7306be99be961c32b7
Ole Morten <cybermoorox@gmail.com>
2018-11-06 13:18:45 +0100

    Revert "Merge branch 'M.-Ali' into ThunderCow"
    This reverts commit 80ac804eea3b8ef5e0c339035ef3086bf3d515e3.

--- README.md
+++ README.md
@@ -1,6 +1,3 @@
 MO-POMA Tech
 
 Use JSP and JSTL
-
-M. Ali (Branch)
-Individual branch to track the work and keep Version Control System.
---
+++ src/java/Servlets/addModule.java
@@ -0,0 +1,97 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.PreparedStatement;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Petr
+ */
+@WebServlet(name = "addModule", urlPatterns = {"/addModule"})
+public class addModule extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+		 try {String query="insert into Module (name,deadline,learning_Goals) values (?,?,?,?)";
+			pst.setString(1,name.getText());
+			pst.setString(2,deadline.getText());
+			pst.setString(3,learning_Goals.getText());
+			
+			pst.execute();
+			
+			JOptionPane.showMessageDialog(null, "Zaměstnanec vytvořen");
+			pst.close();
+			((Node)event.getSource()).getScene().getWindow().hide();
+			
+			
+} catch (SQLException e) {
+	
+	// TODO Auto-generated catch block
+	e.printStackTrace();
+}
+}
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
