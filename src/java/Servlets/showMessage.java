7e14f968918f8791a2acdf14a993155ea0d4a48a
MahamedYusuf-UIA <32332090+MahamedYusuf-UIA@users.noreply.github.com>
2018-11-14 20:34:36 +0100

    Update showMessage.java

--- src/java/Servlets/showMessage.java
+++ src/java/Servlets/showMessage.java
@@ -7,6 +7,7 @@ package Servlets;
 
 import Classes.SqlHandler;
 import Entities.Message;
+import static java.lang.System.out;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.ResultSet;
@@ -19,65 +20,60 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
- * @author Mahamed Salad
+ * @author oddandre
  */
-@WebServlet(name = "showMessage", urlPatterns = {"/showMessage"})
+@WebServlet(name = "showMessages", urlPatterns = {"/showMessages"})
 public class showMessage extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-        protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+   
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException, SQLException {
         response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
+       
+        try (PrintWriter out = response.getWriter()) {  
             
-            sqlHandler sqlHdl = new SqlHandler(out);
-            ResultSet rst = sqlHdl.getMessage();
-            
-            List<Message> messages = new ArrayList();
-            
-            try{
+            SqlHandler sqlHdl = new SqlHandler(out);
+            ResultSet rst = sqlHdl. viewMessages();
+            List <Message> messages = new ArrayList();
+
+            try {
                 int rowCount = 0;
-                
-                while(rst.next()){
-                    Message message = new Message();
-                    
-                    Integer Reciever = rst.getInt("Reciever");
-                    Integer Sender = rst.getInt("Sender");
-                    Integer id = rst.getInt("message_Id");
-                    
-                    message.buildMessageForlist(Reciever, Sender, id);
-                    
-                    messages.add(message);
-                    
-                    ++rowCount;
-                    
-                }
-                
-                catch (SQLException ex){
-                        out.println("Ikke hentet fra DB " +ex);
-                }
-                
-                RequestDispatcher view = request.getRequestDispatcher("/Users/showMessage.jsp");
-                
-                view.forward(request,response);
+                    while(rst.next()) {   // Move the cursor to the next row, return false if no more row
+                       Message messageObj = new Message();
+                      
+                        Integer message_id = rst.getInt("message_Id");
+                        Integer datetime_message = rst.getInt("datetime_Message");
+                        Integer Reciever = rst.getInt("Reciever");
+                        Integer Sender = rst.getInt("sender");
+                        String Subject = rst.getString("Subject");
+                        String Content = rst.getString("Content");
+                        
+                        
+                       
+                        messageObj.messageList(message_id, datetime_message, Reciever, Sender, Subject, Content);
+                        
+                        messages.add(messageObj);
+                        
+                        
+                 
+                        ++rowCount;
+                     } 
             }
-        }
+            catch (SQLException ex){
+                out.println("Error");
+            }
             
-            
-            
-            
-            --------------------------
+            //Put data into the requset for the next page allowing us to use it.
+            request.setAttribute("Messages",messages);
+            //Get the jsp file where we have put our html
+            RequestDispatcher view = request.getRequestDispatcher("/Users/showMessage.jsp");
+            //Send our data from request into the jsp file
+            view.forward(request,response);
             
         }
     }
@@ -86,7 +82,7 @@ public class showMessage extends HttpServlet {
     /**
      * Handles the HTTP <code>GET</code> method.
      *
-     * @param request servlet request
+     * @param request servlet request3
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
@@ -94,8 +90,19 @@ public class showMessage extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        
+        try{
         processRequest(request, response);
     }
+        
+        catch(SQLException ex){
+            
+            Logger.getLogger(showMessage.class.getName()).log(Level.SEVERE, null, ex);
+            //Logger.getLogger(showMessage.class.getName()).log().log(Level.SEVERE, null, ex);
+            
+                
+        }}
+        
 
     /**
      * Handles the HTTP <code>POST</code> method.
@@ -108,8 +115,15 @@ public class showMessage extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        try{
         processRequest(request, response);
-    }
+    }   catch(SQLException ex){
+        
+        Logger.getLogger(showMessage.class.getName()).log(Level.SEVERE, null, ex);
+        
+        //Logger.getLogger(showMessage.class.getName()).log()).log(Level.SEVERE, null, ex);
+                
+    }}
 
     /**
      * Returns a short description of the servlet.
@@ -120,3 +134,5 @@ public class showMessage extends HttpServlet {
     public String getServletInfo() {
         return "Short description";
     }// </editor-fold>
+
+}
