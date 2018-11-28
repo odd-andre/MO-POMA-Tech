cf73a04435c4a5510a4920158640eee61bb7d395
seawaterz <35767365+seawaterz@users.noreply.github.com>
2018-11-28 12:55:40 +0100

    Add files via upload
    updated and ready to merge

--- src/java/Entities/Forum_Post.java
+++ src/java/Entities/Forum_Post.java
@@ -9,6 +9,8 @@ import Classes.SqlHandler;
 import java.io.PrintWriter;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
@@ -16,12 +18,14 @@ import java.sql.SQLException;
  */
 public class Forum_Post extends Forum {
 
-    Integer forum_Id;
-    private Integer forumpost_Id;
-    private String datetime_upload;
-    private String fPostname;
-    private Integer creator;
+    //Integer forum_Id;
+    Integer forumpost_Id;
+    String datetime_upload;
+    String fPostname;
+    Integer creator;
    
+    List<Forum_comment> forumComments = new ArrayList();
+    
     public Forum_Post(){
         /*(Integer forumpost_Id, Integer forum_Id, String datetime_upload, String fPostname, Integer creator){
         this.forumpost_Id = forumpost_Id;
@@ -32,12 +36,29 @@ public class Forum_Post extends Forum {
         */
     }
    
+    public void addCommentArray(String content){
+        Forum_comment fComment = new Forum_comment();
+        
+        fComment.setContent(content);
+        forumComments.add(fComment);
+    }
+     public void setValues(Integer fPostId, String fPostName, Integer creator){
+         this.forumpost_Id=fPostId;
+         //this.datetime_upload=dateTime;
+         this.fPostname=fPostName;
+         this.creator=creator;
+     }
+     public String getfPostName(){return this.fPostname;}
+    
    /* public Integer getforumpost_Id(){return this.forumpost_Id;}
     public Integer getforum_Id(){return this.forum_Id;}
     public String getdatetime_upload(){return this.datetime_upload;}
     public String getfPostname(){return this.fPostname;}
     public Integer getcreator(){return this.creator;}
     */
+     public List getCommentArray(){
+         return forumComments;
+     }
     public void createPost(PrintWriter out, Integer forumpost_Id, Integer forum_Id, String datetime_upload, String fPostname, Integer creator){
        SqlHandler sqlhndl = new SqlHandler(out);
        sqlhndl.createPost(forumpost_Id, forum_Id, datetime_upload, fPostname, creator);
