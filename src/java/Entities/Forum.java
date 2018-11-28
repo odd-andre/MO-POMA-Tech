e9dc029d2418eeb2835feb6533eb1b0eacb53a80
Ole Morten <31931528+ThunderCow@users.noreply.github.com>
2018-11-28 13:14:11 +0100

    Add files via upload
    Updating forum part with entities

--- src/java/Entities/Forum.java
+++ src/java/Entities/Forum.java
@@ -7,8 +7,12 @@ package Entities;
 
 import Classes.SqlHandler;
 import java.io.PrintWriter;
+import static java.lang.System.out;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 /**
  *
@@ -19,6 +23,9 @@ public class Forum {
     Integer forum_Id;
     Integer creator_Id;
     String fName;
+    
+    HashMap<Integer, Forum_Post> comMap = new HashMap();
+    
     
     
     public Forum(){
@@ -33,6 +40,42 @@ public class Forum {
         this.fName=ForumName;
  
     }*/
+    public void getFroumContent(PrintWriter out, Integer id){
+        SqlHandler sqlhnd = new SqlHandler(out);
+       ResultSet rset = sqlhnd.getForumCom(id);
+       try {
+                
+            int rowCount = 0;
+                while(rset.next()) {   // Move the cursor to the next row, return false if no more row
+                    
+                    Integer forumpost_Id= rset.getInt("forumpost_Id");    
+                    String fPostname= rset.getString("fPostname");
+                    Integer creator=rset.getInt("creator");          
+                    String content=rset.getString("content");
+                    Integer poster=rset.getInt("poster");
+                    Integer forum_Id = rset.getInt("forum_Id");
+                    this.fName = rset.getString("fName");    
+                    
+                    if (comMap.containsKey(forumpost_Id)){
+                        comMap.get(forumpost_Id).addCommentArray(content);
+                                
+                    }else{
+                        Forum_Post fPost = new Forum_Post();
+                        fPost.setValues(poster, fPostname, creator);
+                        fPost.addCommentArray(content);
+                        comMap.put(forumpost_Id, fPost);
+                    }
+          
+                    ++rowCount;
+                 }  // end while
+            }
+            catch (SQLException ex) {
+                
+                out.println("Ikke hentet fra DB " +ex);
+            }
+    }
+       
+    
     
     public void getForumDetail(PrintWriter out, Integer id){
         SqlHandler sqlhandl = new SqlHandler(out);
@@ -65,13 +108,15 @@ public class Forum {
    
         public int getFroumID(){return this.forum_Id;}
         public int getCreatorID(){return this.creator_Id;}
-        public String getFroumName(){return this.fName;}
+        public String getForumName(){return this.fName;}
     
        /* public void getForumNumber(PrintWriter out, Integer id){
         SqlHandler sqlhandl = new SqlHandler(out);
         sqlhandl.getForum(id);
      }*/
-    
+       public HashMap getHash(){
+           return comMap;
+       }
        public void createForum(PrintWriter out, Integer forum_Id, Integer creator_Id, String fName){
        SqlHandler sqlhndl = new SqlHandler(out);
        sqlhndl.createForum(forum_Id, creator_Id, fName);
