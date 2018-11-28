90c161aba85704442242aefb43f2e13af2b75bf3
Mahamed <mahamy16@uia.no>
2018-11-28 14:30:13 +0100

    creatMessage finaly complete

--- src/java/Servlets/creatMessage.java
+++ src/java/Servlets/creatMessage.java
@@ -35,7 +35,7 @@ public class creatMessage extends HttpServlet {
      * @throws IOException if an I/O error occurs
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+            throws ServletException, IOException, SQLException {
         response.setContentType("text/html;charset=UTF-8");
         
         
@@ -50,7 +50,31 @@ public class creatMessage extends HttpServlet {
           
         }
     }
-
+    
+                   
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            processRequest(request, response);
+        } catch (SQLException ex){
+            Logger.getLogger(showMessage.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    
+    
+    
+    
     
      /**
              * Handles the HTTP <code>GET</code> method.
@@ -63,8 +87,13 @@ public class creatMessage extends HttpServlet {
                 @Override
                  protected void doPost  (HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-                     processRequest(request, response);
-    } 
+         try {
+            processRequest(request, response);
+        } 
+                      catch (SQLException ex){
+            Logger.getLogger(showMessage.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }   
    
     /**
      * Returns a short description of the servlet.
--- src/java/Servlets/creatMessagePost.java
+++ src/java/Servlets/creatMessagePost.java
@@ -8,10 +8,9 @@ package Servlets;
 import Entities.Message;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.sql.SQLException;
-import java.util.logging.Level;
-import java.util.logging.Logger;    
-import javax.servlet.RequestDispatcher;
+
+    
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -22,7 +21,7 @@ import javax.servlet.http.HttpServletResponse;
  *
  * @author Mahamed Salad
  */
-@WebServlet(urlPatterns = {"/creatMessagePost"})
+@WebServlet(name = "creatMessagePost", urlPatterns = {"/creatMessagePost"})
 public class creatMessagePost extends HttpServlet {
 
     /**
@@ -51,23 +50,37 @@ public class creatMessagePost extends HttpServlet {
             
             Message message = new Message();
             message.creatMessage(out, message_Id,Sender,Reciever,datetime_message,Subject,Content);
-            response.sendRedirect("/MO-POMA_Tech/showMessages");
+            response.sendRedirect("/MO-POMA-Tech/showMessage");
             
 
           
         }
     }
 
+  // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
     
      /**
-             * Handles the HTTP <code>GET</code> method.
-             *
-             * @param request servlet request
-             * @param response servlet response
-             * @throws ServletException if a servlet-specific error occurs
-             * @throws IOException if an I/O error occurs
-             */
-                @Override
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+     @Override
                  protected void doPost  (HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
                      processRequest(request, response);
